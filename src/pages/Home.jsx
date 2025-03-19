9b99a97cbaadaec9424c9b22c2ccdb153aecb85a
pandekartik <pande.kartik@gmail.com>
2025-03-19 16:36:31 +0530

    home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,25 @@
 // import Hero from "../components/Hero";
 import { Link } from 'react-router-dom';
+import bgImage from '../assets/bg/homeHeroBg.png'
 
 
 export default function Home() {
     return (
-        <div className="hero min-h-[50vh] bg-base-200">
-            <div className="hero-content text-center">
+        <div className="hero bg-cover bg-center px-4 py-8 md:px-20 md:py-32"
+            style={{ backgroundImage: `url(${bgImage})` }}>
+            <div className="text-primary-content text-center">
                 <div className="text-left md:text-center">
-                    <h1 className="text-2xl leading-10 md:text-5xl md:leading-relaxed font-bold">
-                        Break Language Barriers with <span className="text-primary">Agentic AI-Powered</span> Multilingual Chatbots
+                    <h1 className="text-3xl leading-10 md:text-5xl md:leading-relaxed font-bold">
+                        Break Language Barriers with Agentic AI-Powered Multilingual Chatbots
                     </h1>
                     <p className="py-6 text-md md:text-xl">
                         Communicate with customers in 100+ languages using intelligent conversational agents that understand context and nuance
                     </p>
-                    <Link to="/waitlist" className="btn btn-primary">
+                    <Link to="/waitlist" className="btn w-full md:w-fit">
                         Join Waitlist
                     </Link>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
